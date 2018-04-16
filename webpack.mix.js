d248d2a9b896f84ee49adbbefc98f41f366ae3fa
Yamin Alam Sarker <yaminalam27@gmail.com>
2018-04-16 12:53:11 +0600

    Implementing Laravel mix and browser sync

---
+++ webpack.mix.js
@@ -0,0 +1,22 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');
+
+// mix.styles(['public/css/style.css', "public/css/theme.css"], 'public/css/all.css');
+
+// mix.js('resources/assets/js/app.js', 'public/js')
+//     .sourceMaps();
+
+mix.browserSync('127.0.0.1:8000');
